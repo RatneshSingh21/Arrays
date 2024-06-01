211a333d840e97ffd62f574f24b9b418d92af265
Ratnesh Singh <153854442+RatneshSingh21@users.noreply.github.com>
2024-06-02 01:39:11 +0530

    Candy.java
    There are n children standing in a line. Each child is assigned a rating value given in the integer array ratings.
    
    You are giving candies to these children subjected to the following requirements:
    
    Each child must have at least one candy.
    Children with a higher rating get more candies than their neighbors.
    Return the minimum number of candies you need to have to distribute the candies to the children.
    
     
    
    Example 1:
    
    Input: ratings = [1,0,2]
    Output: 5
    Explanation: You can allocate to the first, second and third child with 2, 1, 2 candies respectively.
    Example 2:
    
    Input: ratings = [1,2,2]
    Output: 4
    Explanation: You can allocate to the first, second and third child with 1, 2, 1 candies respectively.
    The third child gets 1 candy because it satisfies the above two conditions.

---
+++ Candy.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int candy(int[] ratings) {
+          int n = ratings.length;
+        int[] candies = new int[n];
+        Arrays.fill(candies, 1);    // Initialize all elements to 1
+        // First pass: left to right
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i - 1] && candies[i] <= candies[i - 1]) {
+                candies[i] = candies[i - 1] + 1;
+            }
+        }
+        
+        // Second pass: right to left
+        for (int i = n - 2; i >= 0; i--) {
+            if (ratings[i] > ratings[i + 1] && candies[i] <= candies[i + 1]) {
+                candies[i] = candies[i + 1] + 1;
+            }
+        }
+        
+        // Calculate the total number of candies
+        int totalCandies = 0;
+        for (int i = 0; i < n; i++) {
+            totalCandies += candies[i];
+        }
+        
+        return totalCandies;
+    }
+}
