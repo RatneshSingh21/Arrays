8884cecd3bc5b4064ef5aa8fee394c8bb172f2bb
Ratnesh Singh <153854442+RatneshSingh21@users.noreply.github.com>
2024-07-13 00:56:36 +0530

    FirstMissingPositive.java

---
+++ irstMissingPositive.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int n = nums.length;
+        
+        // Step 1: Place each number in its correct position if possible
+        for (int i = 0; i < n; i++) {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i]) {
+                // Swap nums[i] with nums[nums[i] - 1]
+                int temp = nums[i];
+                nums[i] = nums[temp - 1];
+                nums[temp - 1] = temp;
+            }
+        }
+
+        // Step 2: Find the first index i where nums[i] != i + 1
+        for (int i = 0; i < n; i++) {
+            if (nums[i] != i + 1) {
+                return i + 1;
+            }
+        }
+
+        // If all positions are correct, then the missing number is n + 1
+        return n + 1;
+    }
+}
